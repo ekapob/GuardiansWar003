84c98a42c13e019c1c54cbede0d4803277ceb90d
ekapob <35923406+ekapob@users.noreply.github.com>
2018-04-23 02:43:53 +0700

    set BattleLog Script : wait for sync

--- Assets/Scripts/BattleLogScene/MotherLogScript.cs
+++ Assets/Scripts/BattleLogScene/MotherLogScript.cs
@@ -7,8 +7,12 @@ public class MotherLogScript : MonoBehaviour {
 
 	public static MotherLogScript Instance;
 	public Text[] pTxt;
+	public BattleLogTxt[] battleLogScript;
 	// Use this for initialization
 	void Start () {
+		for (int i = 0; i < PhotonNetwork.playerList.Length - 1; i++) {
+			battleLogScript [i].SetAvtiveLog ();
+		}
 	}
 	
 	// Update is called once per frame
