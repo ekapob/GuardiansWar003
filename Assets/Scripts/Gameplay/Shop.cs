7c720d236ce71c845189a603675377042593037d
ekapob <35923406+ekapob@users.noreply.github.com>
2018-05-02 12:33:11 +0700

    Show Current Unit

--- Assets/Scripts/Gameplay/Shop.cs
+++ Assets/Scripts/Gameplay/Shop.cs
@@ -15,23 +15,29 @@ public class Shop : MonoBehaviour {
 		
 	public void SelectStdTurret()
 	{
-		if(PlayerStats.Money >= 100)
-			buildManager.SelectTurretToBuild (stdTurret,"Turret");
+		if (PlayerStats.Money >= 100) {
+			buildManager.SelectTurretToBuild (stdTurret, "Turret");
+			CurrentTowerPicked.Instance.turretPicShow.texture = CurrentTowerPicked.Instance.turretPic [0];
+		}
 		else
 			PauseAndExitButton.Instance.RunNoGold ();
 	}
 
 	public void SelectMisTurret()
 	{
-		if(PlayerStats.Money >= 500)
-			buildManager.SelectTurretToBuild (misTurret,"MissileLauncher");
+		if (PlayerStats.Money >= 500) {
+			buildManager.SelectTurretToBuild (misTurret, "MissileLauncher");
+			CurrentTowerPicked.Instance.turretPicShow.texture = CurrentTowerPicked.Instance.turretPic [1];
+		}
 		else
 			PauseAndExitButton.Instance.RunNoGold ();
 	}
 	public void SelectLasTurret()
 	{
-		if (PlayerStats.Money >= 225)
+		if (PlayerStats.Money >= 225) {
 			buildManager.SelectTurretToBuild (lasTurret, "LaserBeamer");
+			CurrentTowerPicked.Instance.turretPicShow.texture = CurrentTowerPicked.Instance.turretPic [2];
+		}
 		else
 			PauseAndExitButton.Instance.RunNoGold ();
 	}
